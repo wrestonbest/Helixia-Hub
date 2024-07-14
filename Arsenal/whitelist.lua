78a7a0a048396b15bee7315938f2187c35acc7f8
Toprak <topraqk1@gmail.com>
2024-07-15 02:42:55 +0300

    Create whitelist.lua

---
+++ Arsenal/whitelist.lua
@@ -0,0 +1,12 @@
+local whitelist = {""}
+
+local function whitelisted(playerName)
+    for _, name in ipairs(whitelist) do
+        if name == playerName then
+            return true
+        end
+    end
+    return false
+end
+
+return whitelisted
