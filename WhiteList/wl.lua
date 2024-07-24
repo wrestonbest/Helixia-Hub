9239e29fa1ad999978705a63e0721872a0a3a9db
Toprak <topraqk1@gmail.com>
2024-07-25 01:39:03 +0300

    Update wl.lua

--- WhiteList/wl.lua
+++ WhiteList/wl.lua
@@ -1,9 +1,12 @@
 return function(username)
-    local whitelists = game:HttpGet("https://raw.githubusercontent.com/wrestonbest/Luminary-Hub/main/WhiteList/whitelists")
+    local whitelists = game:HttpGet("https://your_github_repo_url/whitelists")
     local whitelistTable = {}
 
-    for name in whitelists:gmatch("[^,]+") do
-        table.insert(whitelistTable, name)
+    for name in whitelists:gmatch("[^,\r\n]+") do
+        name = name:match("^%s*(.-)%s*$")  -- Trim leading and trailing spaces
+        if #name > 0 then
+            table.insert(whitelistTable, name)
+        end
     end
 
     for _, name in ipairs(whitelistTable) do
